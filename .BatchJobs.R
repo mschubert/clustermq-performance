d173d15146bc7e21bfb4f267ebb186d8fd5866c0
mschubert <mschu.dev@gmail.com>
2017-11-24 23:04:00 +0000

    make sql db lock less likely

--- .BatchJobs.R
+++ .BatchJobs.R
@@ -1,3 +1,5 @@
+# https://github.com/tudo-r/BatchJobs/wiki/Configuration
 cluster.functions = makeClusterFunctionsLSF("BatchJobs.tmpl")
 staged.queries = TRUE
 debug = FALSE
+db.options = list(pragmas = c("busy_timeout=5000", "journal_mode=WAL"))
